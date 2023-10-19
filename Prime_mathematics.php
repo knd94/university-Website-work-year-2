49fe9b42807f3f2a8f75eede59d70bea6a5191fb
knd94 <69738912+knd94@users.noreply.github.com>
2023-10-19 14:42:17 +0100

    Prime Numbers adding
    It is adding the first 3 prime numbers.

---
+++ Prime_mathematics.php
@@ -0,0 +1,9 @@
+<?php
+$prime1 = 1;
+$prime2 = 2;
+$prime3 = 3;
+
+$answeradding = $prime1 + $prime2 + $prime3;
+
+print"Addng these numbers together: $prime1, $prime2 & $prime3 should give you: $answeradding";
+?>

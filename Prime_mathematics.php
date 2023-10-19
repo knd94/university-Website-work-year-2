13bd906fdb16ffed64ace557da89bb242913b95f
knd94 <69738912+knd94@users.noreply.github.com>
2023-10-19 14:49:23 +0100

    Adding and multiplication now
    It is adding and multiplying the first 3 prime numbers.

--- Prime_mathematics.php
+++ Prime_mathematics.php
@@ -3,7 +3,9 @@ $prime1 = 1;
 $prime2 = 2;
 $prime3 = 3;
 
-$answeradding = $prime1 + $prime2 + $prime3;
+$answeradding = $prime1 + $prime2 += $prime3;
+$answermultiplication = $prime1 * $prime2 *= $prime3;
 
 print"Addng these numbers together: $prime1, $prime2 & $prime3 should give you: $answeradding";
+print"<p> And multiplying those numbers should give you: $answermultiplication";
 ?>

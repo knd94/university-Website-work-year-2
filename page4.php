e1fe36748e8f0ad4b11f53af1df45e3556d7378c
knd94 <69738912+knd94@users.noreply.github.com>
2023-11-01 16:48:21 +0000

    Added comments.

--- page4.php
+++ page4.php
@@ -15,12 +15,32 @@
  ?>
  </head>
  <body>
- <h1>What colour is this heading?</h1>
- <form method="post" action="<?= $_SERVER["PHP_SELF"]; ?>">
- <input type="submit" name="style" value="0"><BR>
- <input type="submit" name="style" value="1"><BR>
- <input type="submit" name="style" value="2">
- </form>
- </body>
- </html>
+ <h1>What color is this heading?</h1>
  
+ <!-- Buttons -->
+ <form method="post" action="<?= $_SERVER["PHP_SELF"]; ?>">
+ <input type="submit" name="buttonStyle" value="0"><BR>
+ <input type="submit" name="buttonStyle" value="1"><BR>
+ <input type="submit" name="buttonStyle" value="2">
+ </form>
+ 
+<!-- Radio Buttons and Drop Down menus. -->
+ <form method="post" action="<?= $_SERVER["PHP_SELF"]; ?>">
+        <input type="radio" name="radioStyle" value="0" id="style0">
+        <label for="style0">Style 0</label><br>
+        <input type="radio" name="radioStyle" value="1" id="style1">
+        <label for="style1">Style 1</label><br>
+        <input type="radio" name="radioStyle" value="2" id="style2">
+        <label for="style2">Style 2</label><br>
+
+        <label for="styleSelect">Select a style:</label>
+        <select name="selectStyle" id="styleSelect">
+            <option value="0">Style 0</option>
+            <option value="1">Style 1</option>
+            <option value="2">Style 2</option>
+        </select>
+
+        <input type="submit" value="Apply Style" name="submitButton">
+    </form>
+ </body>
+</html>

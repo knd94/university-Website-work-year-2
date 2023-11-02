c69888fa26bfd16c235de41ddaf2d45aebbcf9ab
knd94 <69738912+knd94@users.noreply.github.com>
2023-11-02 15:38:36 +0000

    Task 1
    It's task 1!

---
+++ 5cs023-task1-2110491.php
@@ -0,0 +1,54 @@
+<?php
+
+// Database connection settings
+$hostname = "";
+$username = "";
+$password = "";
+$database = "";
+
+// Create a new MySQLi connection
+$mysqli = new mysqli($hostname, $username, $password, $database);
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+} else {
+    echo "Connected to the database successfully.";
+}
+
+// Run SQL query
+$query = "SELECT Game_name, Genre, Price, Date_of_rlease FROM videogames";
+$result = $mysqli->query($query);
+
+// Check for query execution errors
+if (!$result) {
+    die("MySQL error: " . $mysqli->error);
+}
+
+// Check if there are any rows returned
+if ($result->num_rows > 0) {
+    echo "<p>" . $result->num_rows . " record(s) were returned...</p>";
+
+    // Display the data in an HTML table
+    echo "<table border='1'>";
+    echo "<tr><th>Game Name</th><th>Genre</th><th>Price</th><th>Release Date</th></tr>";
+    
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['Game_name'] . "</td>";
+        echo "<td>" . $row['Genre'] . "</td>";
+        echo "<td>" . $row['Price'] . "</td>";
+		echo "<td>" . $row['Date_of_rlease'] . "</td>";
+        echo "</tr>";
+    }
+    
+    echo "</table>";
+} else {
+    echo "No records found.";
+}
+
+// Close the database connection
+$mysqli->close();
+
+?>

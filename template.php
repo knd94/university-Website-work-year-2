a7ca42b045d6507a7e5c99d15d9fa39115e55ae4
knd94 <69738912+knd94@users.noreply.github.com>
2023-11-21 21:50:42 +0000

    Twig Website Template
    Using the Twig Website Template.

---
+++ template.php
@@ -0,0 +1,20 @@
+<?php
+
+// Point to library
+require_once 'vendor/autoload.php';
+
+// Set up Environment
+$loader = new \Twig\Loader\FilesystemLoader('.');
+$twig = new \Twig\Environment($loader);
+
+// Array of data
+$people[0]['FirstName'] = "Alix";
+$people[0]['Surname'] = "Bergeret";
+$people[1]['FirstName'] = "Chris";
+$people[1]['Surname'] = "Dennett";
+
+// Load and render template
+echo $twig->render('template.html', 
+                   array('a_variable' => 'Alix', 
+                         'another_variable' => 'Bergeret',
+                         'people' => $people));

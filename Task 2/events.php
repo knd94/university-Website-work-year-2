3e3da588490bb2930ee124567b9ee5150859fcca
knd94 <69738912+knd94@users.noreply.github.com>
2023-11-25 21:24:32 +0000

    Updated events.php, added register_event.php
    Updated the events.php to dynamically change the buttons. Added register_event.php.

--- Task 2/events.php
+++ Task 2/events.php
@@ -3,7 +3,7 @@ session_start();
 include('db.php');
 
 // Fetch all events
-$sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.username, u.profile_picture
+$sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.id as user_id, u.username, u.profile_picture
         FROM events e
         JOIN users u ON e.user_id = u.id
         ORDER BY e.event_date ASC";
@@ -14,7 +14,7 @@ $today = date("Y-m-d");
 $deleteSql = "DELETE FROM events WHERE event_date < '$today'";
 $conn->query($deleteSql);
 
-// Display events
+// Display all events
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,11 +27,14 @@ $conn->query($deleteSql);
 <body>
     <?php
     while ($row = $result->fetch_assoc()) {
+        // Display individual events
         ?>
         <div>
-            <!-- Display user information (username and profile picture) -->
-            <img src="<?php echo $row['profile_picture']; ?>" alt="Profile Picture" width="50">
-            <strong><?php echo $row['username']; ?></strong>
+            <!-- Link to user profile page -->
+            <a href="user_profile.php?user_id=<?php echo $row['user_id']; ?>">
+                <img src="<?php echo $row['profile_picture']; ?>" alt="Profile Picture" width="50">
+                <strong><?php echo $row['username']; ?></strong>
+            </a>
 
             <!-- Display event information -->
             <h3><?php echo $row['title']; ?></h3>
@@ -41,11 +44,45 @@ $conn->query($deleteSql);
             <!-- Display event picture -->
             <img src="<?php echo $row['image_path']; ?>" alt="Event Picture" width="300">
 
-            <!-- Add more styling and HTML structure as needed -->
+            <!-- Registration form -->
+<?php
+if (isset($_SESSION['username'])) {
+    $event_id = $row['id'];
+    $username = $_SESSION['username'];
+
+    // Fetch the user ID based on the username
+    $getUserIDSql = "SELECT id FROM users WHERE username = '$username'";
+    $getUserIDResult = $conn->query($getUserIDSql);
+
+    if ($getUserIDResult && $getUserIDResult->num_rows > 0) {
+        $user_id = $getUserIDResult->fetch_assoc()['id'];
+
+        // Check if the user is already registered for the event
+        $checkSql = "SELECT * FROM event_registrations WHERE event_id = $event_id AND user_id = $user_id";
+        $checkResult = $conn->query($checkSql);
+
+        // Button text based on registration status
+        $buttonText = $checkResult && $checkResult->num_rows > 0 ? "Unregister from Event" : "Register for Event";
+        $formAction = "register_event.php";
+
+        ?>
+        <!-- Registration form with dynamic button text -->
+        <form method="post" action="<?php echo $formAction; ?>">
+            <input type="hidden" name="event_id" value="<?php echo $event_id; ?>">
+            <input type="submit" name="register_event" value="<?php echo $buttonText; ?>">
+        </form>
+        <?php
+    } else {
+        echo "User not found or error in fetching user ID.";
+    }
+} else {
+    echo "Username not found in session. Please log in.";
+}
+?>
+<!-- Add more styling and HTML structure as needed -->
         </div>
         <?php
     }
     ?>
-
 </body>
 </html>
---
+++ Task 2/register_event.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+var_dump($_SESSION); // Output session variables for debugging
+
+include('db.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register_event'])) {
+    // Check if 'username' key exists in the $_SESSION array
+    if (isset($_SESSION['username'])) {
+        $event_id = $_POST['event_id'];
+        $username = $_SESSION['username'];
+
+        // Fetch the user ID based on the username
+        $getUserIDSql = "SELECT id FROM users WHERE username = '$username'";
+        $getUserIDResult = $conn->query($getUserIDSql);
+
+        if ($getUserIDResult) {
+            if ($getUserIDResult->num_rows > 0) {
+                $row = $getUserIDResult->fetch_assoc();
+                $user_id = $row['id'];
+
+                // Check if the user is already registered for the event
+                $checkSql = "SELECT * FROM event_registrations WHERE event_id = $event_id AND user_id = $user_id";
+                $checkResult = $conn->query($checkSql);
+
+                if ($checkResult) {
+                    if ($checkResult->num_rows == 0) {
+                        // If not registered, insert into the event_registrations table
+                        $insertSql = "INSERT INTO event_registrations (event_id, user_id) VALUES ($event_id, $user_id)";
+                        if ($conn->query($insertSql)) {
+                            echo "Successfully registered for the event!";
+                        } else {
+                            echo "Error: " . $conn->error;
+                        }
+                    } else {
+                        // User is already registered, provide option to unregister
+                        $unregisterSql = "DELETE FROM event_registrations WHERE event_id = $event_id AND user_id = $user_id";
+                        if ($conn->query($unregisterSql)) {
+                            echo "Successfully unregistered from the event!";
+                        } else {
+                            echo "Error: " . $conn->error;
+                        }
+                    }
+                } else {
+                    echo "Error: " . $conn->error;
+                }
+
+                // Redirect back to events.php after registration or unregistration
+                header("Location: events.php");
+                exit;
+            } else {
+                echo "User not found.";
+            }
+        } else {
+            echo "Error: " . $conn->error;
+        }
+    } else {
+        echo "Username not found in session. Please log in.";
+    }
+}
+?>

71078520e75f343a5a544e2fc400efcbfe761931
knd94 <69738912+knd94@users.noreply.github.com>
2023-11-29 00:54:48 +0000

    Updated events.php
    With AJAX autocomplete!

--- Task 2/events.php
+++ Task 2/events.php
@@ -21,65 +21,50 @@ function getButtonText($filterOption, $eventId, $registeredEvents) {
 
 // Initialize variables
 $username = isset($_SESSION['username']) ? $_SESSION['username'] : null;
-$today = date("Y-m-d");
 
 // Handle filter form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['filter_events'])) {
-    $filterOption = $_POST['filter_option'];
+$filterOption = isset($_GET['filter_option']) ? $_GET['filter_option'] : 'all_events';
+$searchTerm = isset($_GET['search_term']) ? $_GET['search_term'] : '';
 
-    // Set a cookie to remember the user's filter preference
-    setcookie('filter_option', $filterOption, time() + (30 * 24 * 60 * 60), '/');
-} else {
-    // Use the cookie value if available
-    $filterOption = isset($_COOKIE['filter_option']) ? $_COOKIE['filter_option'] : 'all_events';
-}
+// Set a cookie to remember the user's filter preference
+setcookie('filter_option', $filterOption, time() + (30 * 24 * 60 * 60), '/');
 
-// Fetch events based on the filter option
+// Fetch events based on the filter option and search term
+$sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.id as user_id, u.username, u.profile_picture
+        FROM events e
+        JOIN users u ON e.user_id = u.id";
+
 switch ($filterOption) {
     case 'your_events':
-        $sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.id as user_id, u.username, u.profile_picture
-                FROM events e
-                JOIN users u ON e.user_id = u.id
-                WHERE u.username = '$username'
-                ORDER BY e.event_date ASC";
+        $sql .= " WHERE u.username = '$username'";
         break;
 
     case 'registered_events':
-        $sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.id as user_id, u.username, u.profile_picture
-                FROM events e
-                JOIN users u ON e.user_id = u.id
-                WHERE EXISTS (
+        $sql .= " WHERE EXISTS (
                     SELECT 1 FROM event_registrations er
                     WHERE er.event_id = e.id AND er.username = '$username'
-                )
-                ORDER BY e.event_date ASC";
+                )";
         break;
 
     case 'others_events':
-        $sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.id as user_id, u.username, u.profile_picture
-                FROM events e
-                JOIN users u ON e.user_id = u.id
-                WHERE u.username <> '$username'
-                ORDER BY e.event_date ASC";
+        $sql .= " WHERE u.username <> '$username'";
         break;
-
-    default:
-        $sql = "SELECT e.id, e.title, e.description, e.event_date, e.image_path, u.id as user_id, u.username, u.profile_picture
-                FROM events e
-                JOIN users u ON e.user_id = u.id
-                ORDER BY e.event_date ASC";
 }
+
+if (!empty($searchTerm)) {
+    $sql .= " AND (e.title LIKE '%$searchTerm%' OR e.description LIKE '%$searchTerm%' OR u.username LIKE '%$searchTerm%')";
+}
+
+$sql .= " ORDER BY e.event_date ASC";
 
 // Fetch registered events for the user
 $registeredEvents = array();
-if ($filterOption === 'registered_events' || $filterOption === 'others_events' || $filterOption === 'all_events') {
-    $getRegisteredEventsSql = "SELECT event_id FROM event_registrations WHERE username = '$username'";
-    $getRegisteredEventsResult = $conn->query($getRegisteredEventsSql);
+$getRegisteredEventsSql = "SELECT event_id FROM event_registrations WHERE username = '$username'";
+$getRegisteredEventsResult = $conn->query($getRegisteredEventsSql);
 
-    if ($getRegisteredEventsResult) {
-        while ($row = $getRegisteredEventsResult->fetch_assoc()) {
-            $registeredEvents[] = $row['event_id'];
-        }
+if ($getRegisteredEventsResult) {
+    while ($row = $getRegisteredEventsResult->fetch_assoc()) {
+        $registeredEvents[] = $row['event_id'];
     }
 }
 ?>
@@ -90,11 +75,35 @@ if ($filterOption === 'registered_events' || $filterOption === 'others_events' |
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Events</title>
-    <!-- Include any additional stylesheets or scripts if needed -->
+    <!-- Add this script for autocomplete functionality -->
+    <script>
+        function handleAutocomplete() {
+            var searchInput = document.getElementById('search_term');
+            var resultsContainer = document.getElementById('event_results');
+
+            if (searchInput.value.length >= 1) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        console.log('Response:', xhr.responseText);
+                        if (xhr.status === 200) {
+                            resultsContainer.innerHTML = xhr.responseText;
+                        } else {
+                            console.error('Error:', xhr.statusText);
+                        }
+                    }
+                };
+                xhr.open('GET', 'search_suggestions.php?search_term=' + searchInput.value, true);
+                xhr.send();
+            } else {
+                resultsContainer.innerHTML = '';
+            }
+        }
+    </script>
 </head>
 <body>
     <!-- Filter form -->
-    <form method="post" action="">
+    <form method="get" action="">
         <label for="filter_option">Filter Events:</label>
         <select name="filter_option" id="filter_option">
             <option value="your_events" <?php echo ($filterOption === 'your_events') ? 'selected' : ''; ?>>Your Events</option>
@@ -102,44 +111,45 @@ if ($filterOption === 'registered_events' || $filterOption === 'others_events' |
             <option value="others_events" <?php echo ($filterOption === 'others_events') ? 'selected' : ''; ?>>Other People's Events</option>
             <option value="all_events" <?php echo ($filterOption === 'all_events') ? 'selected' : ''; ?>>All Events</option>
         </select>
-        <input type="submit" name="filter_events" value="Filter">
+        <input type="text" name="search_term" id="search_term" oninput="handleAutocomplete()" placeholder="Search events">
+        <input type="submit" value="Search">
     </form>
 
-    <?php
-    $result = $conn->query($sql);
-
-    while ($row = $result->fetch_assoc()) {
-        // Display individual events
-        ?>
-        <div>
-            <!-- Link to user profile page -->
-            <a href="user_profile.php?user_id=<?php echo $row['user_id']; ?>">
-                <img src="<?php echo $row['profile_picture']; ?>" alt="Profile Picture" width="50">
-                <strong><?php echo $row['username']; ?></strong>
-            </a>
-
-            <!-- Display event information -->
-            <h3><?php echo $row['title']; ?></h3>
-            <p><?php echo $row['description']; ?></p>
-            <p>Date: <?php echo $row['event_date']; ?></p>
-
-            <!-- Display event picture -->
-            <img src="<?php echo $row['image_path']; ?>" alt="Event Picture" width="300">
-
-            <!-- Registration form -->
-<?php
-$buttonText = getButtonText($filterOption, $row['id'], $registeredEvents);
-$formAction = ($filterOption === 'your_events') ? 'edit_event.php?event_id=' . $row['id'] : 'register_event.php';
-?>
-<form method="post" action="<?php echo $formAction; ?>">
-    <input type="hidden" name="event_id" value="<?php echo $row['id']; ?>">
-    <input type="submit" name="<?php echo ($filterOption === 'your_events') ? 'edit_event' : 'register_event'; ?>" value="<?php echo $buttonText; ?>">
-</form>
-        </div>
+    <div id="event_results">
         <?php
-    }
-    ?>
+        $result = $conn->query($sql);
 
-    <!-- Add more styling and HTML structure as needed -->
+        while ($row = $result->fetch_assoc()) {
+            // Display individual events
+            ?>
+            <div>
+                <!-- Link to user profile page -->
+                <a href="user_profile.php?user_id=<?php echo $row['user_id']; ?>">
+                    <img src="<?php echo $row['profile_picture']; ?>" alt="Profile Picture" width="50">
+                    <strong><?php echo $row['username']; ?></strong>
+                </a>
+
+                <!-- Display event information -->
+                <h3><?php echo $row['title']; ?></h3>
+                <p><?php echo $row['description']; ?></p>
+                <p>Date: <?php echo $row['event_date']; ?></p>
+
+                <!-- Display event picture -->
+                <img src="<?php echo $row['image_path']; ?>" alt="Event Picture" width="300">
+
+                <!-- Registration form -->
+                <?php
+                $buttonText = getButtonText($filterOption, $row['id'], $registeredEvents);
+                $formAction = ($filterOption === 'your_events') ? 'edit_event.php?event_id=' . $row['id'] : 'register_event.php';
+                ?>
+                <form method="post" action="<?php echo $formAction; ?>">
+                    <input type="hidden" name="event_id" value="<?php echo $row['id']; ?>">
+                    <input type="submit" name="<?php echo ($filterOption === 'your_events') ? 'edit_event' : 'register_event'; ?>" value="<?php echo $buttonText; ?>">
+                </form>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
 </body>
 </html>

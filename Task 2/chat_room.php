687f9d7d68b353e3920ca36419f6c14488387a06
knd94 <69738912+knd94@users.noreply.github.com>
2023-11-30 15:03:30 +0000

    Updated chat_room.php
    Updated it to add a page title.

--- Task 2/chat_room.php
+++ Task 2/chat_room.php
@@ -8,8 +8,9 @@ include('db.php');
 // Include database functions
 require_once('db_functions.php');
 
-// Retrieve chat room ID from the URL
+// Retrieve chat room ID and friend ID from the URL
 $chatRoomId = isset($_GET['chat_room_id']) ? $_GET['chat_room_id'] : null;
+$friendId = isset($_GET['friend_id']) ? $_GET['friend_id'] : null;
 
 // Ensure the user is logged in
 if (!isset($_SESSION['username'])) {
@@ -37,7 +38,15 @@ function getChatMessages($conn, $chatRoomId) {
 // Retrieve chat messages for the current chat room
 $chatMessages = getChatMessages($conn, $chatRoomId);
 
-// Display chat messages
+// Get the username of the friend for the title
+$friendUsername = getUsernameById($conn, $friendId);
+
+// Set the page title dynamically
+$pageTitle = "Chatting with $friendUsername";
+
+// Display chat messages and update the title
+echo "<h2>$pageTitle</h2>";
+
 foreach ($chatMessages as $message) {
     echo '<p>' . $message['username'] . ': ' . $message['message'] . '</p>';
 }
@@ -58,8 +67,8 @@ function insertChatMessage($conn, $chatRoomId, $userId, $message) {
     <input type="text" name="message" placeholder="Type your message...">
     <input type="submit" name="send_message" value="Send">
 </form>
-<?php
 
+<?php
 // Handle the form submission to send a new message
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['send_message'])) {
     $message = $_POST['message'];
@@ -69,7 +78,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['send_message'])) {
     insertChatMessage($conn, $chatRoomId, $userId, $message);
 
     // Refresh the page to show the new message
-    header("Location: chat_room.php?chat_room_id=" . $chatRoomId);
+    header("Location: chat_room.php?chat_room_id=$chatRoomId&friend_id=$friendId");
     exit();
 }
 ?>
+
+<a href="dashboard.php">Go to Dashboard</a>
+
+<script>
+    // Set the document title using JavaScript
+    document.title = "<?php echo $pageTitle; ?>";
+</script>
